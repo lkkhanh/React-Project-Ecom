d9750323420bd95c5acee6b932293225588f6a96
lkkhanh2000 <1851050060khanh@ou.edu.vn>
2024-05-29 19:53:46 +0700

    a

---
+++ Clone E-Commerce/frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque fuga ipsam aperiam fugiat quaerat voluptas numquam praesentium at unde culpa. Expedita provident, aut rem ullam laudantium voluptatum ex odit commodi?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis omnis harum voluptatum accusantium facilis sapiente et, aspernatur modi molestias, illo dolores voluptatibus. Eveniet cum alias autem facere molestiae assumenda! Magnam minus ratione similique! Possimus atque totam, harum nobis, optio exercitationem quibusdam accusamus incidunt facilis ipsam neque unde non ratione obcaecati.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
